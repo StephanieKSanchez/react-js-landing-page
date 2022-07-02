c78bf8ffabc55b6d94106c9179c46f00a163ab0a
Stephanie Sanchez <stephk006@gmail.com>
2022-07-01 22:48:34 -0400

    begin footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 function App() {
   return (
     <div className="min-h-screen flex flex-col text-white">
-      <h1>Hello, World!</h1>
+      <main className="container mx-auto px-6 pt-16 flex-1 text-center"></main>
+      <footer className="container mx-auto p-6 flex flex-col md:flex-row items-center justify-between">
+        <p>by Stephanie Sanchez</p>
+      </footer>
+      <div className="flex -mx-6">
+        <a href="#" className="mx-3 hover:opacity-80 duration-150">
+          About us
+        </a>{" "}
+        |
+        <a href="#" className="mx-3 hover:opacity-80 duration-150">
+          Privacy
+        </a>{" "}
+        |
+        <a href="#" className="mx-3 hover:opacity-80 duration-150">
+          Contact
+        </a>
+      </div>
     </div>
   );
 }
