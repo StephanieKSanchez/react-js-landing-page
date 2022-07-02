e15a6ab871601740975c3892137b4c94e04ce479
Stephanie Sanchez <stephk006@gmail.com>
2022-07-01 23:02:33 -0400

    adjsuted footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@ function App() {
   return (
     <div className="min-h-screen flex flex-col text-white">
       <main className="container mx-auto px-6 pt-16 flex-1 text-center"></main>
-      <footer className="container mx-auto p-6 flex flex-col md:flex-row items-center justify-between">
-        <p>by Stephanie Sanchez</p>
+      <footer className="container mx-auto p-6">
+        <div className="flex flex-col md:flex-row items-center justify-between">
+          <p className="mb-4 md:mb-0 md:text-xl">by Stephanie Sanchez</p>
+          <div className="container -mx-6">
+            <a href="#" className="mx-3 hover:opacity-80 duration-150">
+              About us
+            </a>{" "}
+            |
+            <a href="#" className="mx-3 hover:opacity-80 duration-150">
+              Privacy
+            </a>{" "}
+            |
+            <a href="#" className="mx-3 hover:opacity-80 duration-150">
+              Contact
+            </a>
+          </div>
+        </div>
       </footer>
-      <div className="flex -mx-6">
-        <a href="#" className="mx-3 hover:opacity-80 duration-150">
-          About us
-        </a>{" "}
-        |
-        <a href="#" className="mx-3 hover:opacity-80 duration-150">
-          Privacy
-        </a>{" "}
-        |
-        <a href="#" className="mx-3 hover:opacity-80 duration-150">
-          Contact
-        </a>
-      </div>
     </div>
   );
 }
