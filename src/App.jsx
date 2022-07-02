9ed37914ede9adea24ef0b38b6ee864b84c4775b
Stephanie Sanchez <stephk006@gmail.com>
2022-07-01 23:32:19 -0400

    added subscribers count

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ function App() {
         <h1 className="text-3xl md:text-6xl lg:text-8xl font-black uppercase mb-8">
           The coding club
         </h1>
+        <div className="lexr-lg md:text-2xl lg:text-3xl py-2 px-4 md:py-4 md:px-10 lg:py-6 lg:px-12 bg-white bg-opacity-10 w-fit mx-auto mb-8 rounded-full">
+          20,120 members
+        </div>
       </main>
       <footer className="container mx-auto p-6">
         <div className="flex flex-col md:flex-row items-center justify-between">
@@ -15,11 +18,11 @@ function App() {
           <div className="container -mx-6">
             <a href="#" className="mx-3 hover:opacity-80 duration-150">
               About us
-            </a>{" "}
+            </a>
             |
             <a href="#" className="mx-3 hover:opacity-80 duration-150">
               Privacy
-            </a>{" "}
+            </a>
             |
             <a href="#" className="mx-3 hover:opacity-80 duration-150">
               Contact
