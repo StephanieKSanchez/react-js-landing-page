d3030aa890238991438b2e2eb6b03455fdbdd4a2
Stephanie Sanchez <stephk006@gmail.com>
2022-07-05 18:55:45 -0400

    styled form

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,17 @@ function App() {
         >
           <div className="flex flex-col md:flex-row justify-center mb-4">
             <input
-              className="revue-form-field"
+              className=" text-lg md:text-2xl placeholder:text-gray-400 placeholder:italic py-4 px-6 md:px-10 lg:py-6 lg:px-12 bg-white bg-opacity-10 focus:bg-opacity-20 duration-150 rounded-full md:rounded-tr-none md:rounded-br-none mb-4 md:mb-0 outline-none"
               placeholder="Your email address..."
               type="email"
               name="member[email]"
             />
-            <input type="submit" value="Join today" name="member[subscribe]" />
+            <input
+              type="submit"
+              value="Join today"
+              name="member[subscribe]"
+              className="bg-primary rounded-full md:rounded-tl-none md:rounded-bl-none text-lg md:text-2xl py-4 px-6 md:px-10 lg:py-6 lg:px-12 cursor-pointer hover:opacity-75 duration-150"
+            />
           </div>
           <div className="opacity-75 italic">
             By subscribing, you agree with Revue's
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js, jsx}"],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {
       colors: {
